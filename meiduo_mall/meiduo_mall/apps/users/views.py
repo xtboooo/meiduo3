50820e1089e3fcbe9af7fc16633da9c4840cac12
xtbo97 <xtbo9719@gmail.com>
2020-12-17 15:05:04 +0800

    注册业务功能实现-用户名是否重复注册

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,1 +1,20 @@
+from django.http import JsonResponse
+from django.views import View
+from users.models import User
 
+
+# GET /usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/
+class UsernameCountView(View):
+    def get(self, request, username):
+        """ 判断注册用户名是否重复 """
+        # 1.查询数据可以判断username是否存在
+        try:
+            count = User.objects.filter(username=username).count()
+        except Exception as e:
+            # print(e)
+            return JsonResponse({'code': 400,
+                                 'message': '操作数据库失败'})
+
+        return JsonResponse({'code': 0,
+                             'message': 'OK',
+                             'count': count,})
