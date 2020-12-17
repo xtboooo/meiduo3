736e7b1b61a309013a39cd003834cf4ef1c2f5e3
xtbo97 <xtbo9719@gmail.com>
2020-12-17 22:12:05 +0800

    注册-用户注册信息保存+CSRF保护验证

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,6 +1,16 @@
+import json
+import re
+
 from django.http import JsonResponse
+from django.middleware.csrf import get_token
 from django.views import View
+from django_redis import get_redis_connection
+
 from users.models import User
+
+import logging
+
+logger = logging.getLogger('django')
 
 
 # GET /usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/
@@ -34,3 +44,79 @@ class MobileCountView(View):
         return JsonResponse({'code': 0,
                              'message': 'OK',
                              'count': count})
+
+
+# POST /register/
+class RegisterView(View):
+    def post(self, request):
+        """ 注册用户信息保存 """
+        # 1.获取参数并进行保存
+        req_data = json.loads(request.body)
+        username = req_data.get('username')
+        password = req_data.get('password')
+        password2 = req_data.get('password2')
+        mobile = req_data.get('mobile')
+        allow = req_data.get('allow')
+        sms_code = req_data.get('sms_code')
+
+        if not all([username, password, password2, mobile, allow, sms_code]):
+            return JsonResponse({'code': 400,
+                                 'message': '缺少必传参数'})
+
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return JsonResponse({'code': 400,
+                                 'message': 'username格式错误!'})
+
+        if not re.match(r'^[a-zA-Z0-9]{8,20}$', password):
+            return JsonResponse({'code': 400,
+                                 'message': 'password格式错误!'})
+
+        if password != password2:
+            return JsonResponse({'code': 400,
+                                 'message': '两次密码不一致!'})
+
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({'code': 400,
+                                 'message': '手机号格式错误!'})
+
+        if not allow:
+            return JsonResponse({'code': 400,
+                                 'message': '请统一协议'})
+
+        # 短信验证码检验
+        redis_conn = get_redis_connection('verify_code')
+        sms_code_redis = redis_conn.get('sms_%s' % mobile)
+
+        if not sms_code_redis:
+            return JsonResponse({'code': 400,
+                                 'message': '短信验证码过期'})
+
+        if sms_code != sms_code_redis:
+            return JsonResponse({'code': 400,
+                                 'message': '短信验证码错误!'})
+
+        # 保存新增用户数据到数据库
+        try:
+            user = User.objects.create_user(username=username,
+                                            password=password,
+                                            mobile=mobile, )
+        except Exception as e:
+            logger.error(e)
+            return JsonResponse({'code': 400,
+                                 'message': '数据库保存错误!'})
+        # 返回响应数据
+        return JsonResponse({'code': 0,
+                             'message': 'OK'})
+
+
+# GET /csrf_token/
+class CSRFTokenView(View):
+    def get(self, request):
+        """ 获取csrf_token的值"""
+        # 1.生成csrf_token的值
+        csrf_token = get_token(request)
+
+        # 2.将csrf_token的值返回
+        return JsonResponse({'code': 400,
+                             'message': 'OK',
+                             'csrf_token': csrf_token})
