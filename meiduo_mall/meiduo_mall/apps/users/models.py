50be64118d76e69d181264d35078d0c7ec45903a
xtbo97 <xtbo9719@gmail.com>
2020-12-20 20:36:59 +0800

    用户中心-个人用户信息-模型字段添加

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -8,6 +8,10 @@ class User(AbstractUser):
 
     mobile = models.CharField(max_length=11, unique=True, verbose_name='电话号')
 
+    # 增加email_active字段
+    # 用于记录邮箱是否激活,默认为False:未激活
+    email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+
     class Meta:
         # 表名
         db_table = 'tb_users'
