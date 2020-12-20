d616c611958dbaf7fd3f9c6c903cd738dc44f287
xtbo97 <xtbo9719@gmail.com>
2020-12-20 11:00:47 +0800

    QQ登陆-绑定QQ登录用户

--- meiduo_mall/meiduo_mall/apps/oauth/utils.py
+++ meiduo_mall/meiduo_mall/apps/oauth/utils.py
@@ -2,7 +2,7 @@
 xtbo97
 """
 from django.conf import settings
-from itsdangerous import TimedJSONWebSignatureSerializer
+from itsdangerous import TimedJSONWebSignatureSerializer, BadData
 
 
 def generate_secret_openid(openid):
@@ -10,10 +10,10 @@ def generate_secret_openid(openid):
     # settings.SECRET_KEY: 加密使用的秘钥
     # 解密过期时间: 10min = 600s
     serializer = TimedJSONWebSignatureSerializer(settings.SECRET_KEY,
-                                                 expires_in=600,)
+                                                 expires_in=600, )
 
     # 待加密的数据
-    data = {'openid':openid}
+    data = {'openid': openid}
 
     # 数据加密操作
     secret_openid = serializer.dumps(data).decode()
@@ -22,3 +22,17 @@ def generate_secret_openid(openid):
     return secret_openid
 
 
+def check_secret_openid(secret_openid):
+    """ 对加密的 openid 进行解密 """
+    # 创建对象
+    serializer = TimedJSONWebSignatureSerializer(settings.SECRET_KEY)
+
+    try:
+        # 进行解密操作
+        data = serializer.loads(secret_openid)
+    except BadData:
+        # 解密出错,返回None
+        return None
+    else:
+        # 获取解密之后的openid并返回
+        return data.get('openid')
--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 from QQLoginTool.QQtool import OAuthQQ
 from django.conf import settings
 from django.contrib.auth import login
@@ -6,8 +9,11 @@ from django.views import View
 
 import logging
 
+from django_redis import get_redis_connection
+
 from oauth.models import OAuthQQUser
-from oauth.utils import generate_secret_openid
+from oauth.utils import generate_secret_openid, check_secret_openid
+from users.models import User
 
 logger = logging.getLogger('django')
 
@@ -77,3 +83,82 @@ class QQUserView(View):
 
             # 返回响应
             return response
+
+    def post(self, request):
+        """ 绑定QQ登陆用户信息"""
+        # 1.获取参数并进行校验
+        req_data = json.loads(request.body)
+
+        mobile = req_data.get('mobile')
+        password = req_data.get('password')
+        sms_code = req_data.get('sms_code')
+        secret_openid = req_data.get('secret_openid')
+
+        if not all([mobile, password, sms_code, secret_openid]):
+            return JsonResponse({'code': 400,
+                                 'message': '缺少必传参数!'})
+
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return JsonResponse({'code': 400,
+                                 'message': '请输入正确的手机号码!'})
+
+        # 判断密码是否合格
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return JsonResponse({'code': 400,
+                                 'message': '请输入8-20位的密码!'})
+
+        # 短信验证码是否正确
+        redis_conn = get_redis_connection('verify_code')
+        sms_code_redis = redis_conn.get('sms_%s' % mobile)
+        if not sms_code_redis:
+            return JsonResponse({'code': 400,
+                                 'message': '短信验证码已过期!'})
+
+        if sms_code != sms_code_redis:
+            return JsonResponse({'code': 400,
+                                 'message': '短信验证码错误!'})
+
+        # 对openid进行解密
+        openid = check_secret_openid(secret_openid)
+
+        if not openid:
+            return JsonResponse({'code': 400,
+                                 'message': 'secret_openid有误!'})
+
+        # 2. 绑定QQ登陆用户信息
+        try:
+            user = User.objects.get(mobile=mobile)
+        except User.DoesNotExist:
+            # 注册未绑定用户
+            import base64
+            # 手机号进行base64编码生成用户名
+            username = base64.b64encode(mobile.encode()).decode()
+            user = User.objects.create_user(username=username,
+                                            password=password,
+                                            mobile=mobile, )
+
+        else:
+            # 校验密码是否正确
+            if not user.check_password(password):
+                return JsonResponse({'code': 400,
+                                     'message': '登陆密码错误!'})
+
+        try:
+            OAuthQQUser.objects.create(openid=openid,
+                                       user_id=user.id)
+        except Exception as e:
+            logger.error(e)
+            return JsonResponse({'code': 400,
+                                 'message': '数据库操作失败!'})
+
+        # 3. 返回响应,登陆成功
+        login(request, user)
+
+        response = JsonResponse({'code': 0,
+                                 'message': 'OK', })
+
+        # 设置cookie
+        response.set_cookie('username', user.username,
+                            max_age=14 * 24 * 3600)
+        return response
