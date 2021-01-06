cf41e8120add75db7c3a62ba4394ba166520b59d
xtbo97 <xtbo9719@gmail.com>
2021-01-06 16:22:09 +0800

    订单创建-订单数据保存-基本功能API

--- meiduo_mall/meiduo_mall/apps/orders/models.py
+++ meiduo_mall/meiduo_mall/apps/orders/models.py
@@ -33,33 +33,6 @@ class OrderInfo(BaseModel):
     def __str__(self):
         return self.order_id
 
-    class OrderInfo(BaseModel):
-        """订单信息"""
-        order_id = models.CharField(max_length=64, primary_key=True,
-                                    verbose_name="订单号")
-        # 一对多关联属性
-        user = models.ForeignKey(User, related_name="orders",
-                                 on_delete=models.PROTECT, verbose_name="下单用户")
-        # 一对多关联属性
-        address = models.ForeignKey(Address, on_delete=models.PROTECT,
-                                    verbose_name="收货地址")
-        total_count = models.IntegerField(default=1, verbose_name="商品总数")
-        total_amount = models.DecimalField(max_digits=10, decimal_places=2,
-                                           verbose_name="商品总金额")
-        freight = models.DecimalField(max_digits=10, decimal_places=2,
-                                      verbose_name="运费")
-        # 1：货到付款 2：支付宝
-        pay_method = models.SmallIntegerField(default=1, verbose_name="支付方式")
-        # 1：待支付 2：待发货 3：待收货 4：待评价 5：已完成 6：已取消
-        status = models.SmallIntegerField(default=1, verbose_name="订单状态")
-
-        class Meta:
-            db_table = "tb_order_info"
-            verbose_name = '订单基本信息'
-
-        def __str__(self):
-            return self.order_id
-
 
 class OrderGoods(BaseModel):
     """订单商品"""
--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -1,11 +1,14 @@
+import json
 from decimal import Decimal
 
 from django.http import JsonResponse
+from django.utils import timezone
 from django.views import View
 
 from meiduo_mall.utils.mixins import LoginRequiredMixin
 from carts.utils import CartHelper
 from goods.models import SKU
+from orders.models import OrderInfo, OrderGoods
 from users.models import Address
 
 
@@ -73,3 +76,110 @@ class OrderSettlementView(LoginRequiredMixin, View):
         return JsonResponse({'code': 0,
                              'message': 'OK',
                              'context': context})
+
+
+# POST /orders/commit/
+class OrderCommitView(LoginRequiredMixin, View):
+    def post(self, request):
+        """订单创建"""
+        # ① 获取参数并进行校验
+        req_data = json.loads(request.body)
+        address_id = req_data.get('address_id')
+        pay_method = req_data.get('pay_method')
+
+        if not all([address_id, pay_method]):
+            return JsonResponse({'code': 400, 'message': '缺少必传参数'})
+
+        # 地址是否存在
+        try:
+            address = Address.objects.get(id=address_id, is_delete=False, user=request.user)
+        except Address.DoesNotExist:
+            return JsonResponse({'code': 400, 'message': '地址信息有误'})
+
+        # 1：货到付款 2：支付宝
+        if pay_method not in (1, 2):
+            return JsonResponse({'code': 400, 'message': '支付方式有误'})
+
+        # ② 组织订单数据
+        user = request.user
+        # 生成订单id
+        order_id = timezone.now().strftime('%Y%m%d%H%M%S') + '%09d' % user.id
+
+        # total_count和total_amount
+        total_count = 0
+        total_amount = 0
+        # 订单状态
+        if pay_method == 1:
+            # 货到付款：待发货
+            status = 2
+        else:
+            # 支付宝：待支付
+            status = 1
+
+        # 运费(此处固定)
+        freight = Decimal(10.00)
+
+        # ③ 向 tb_order_info 数据表中添加一行记录
+        try:
+            order = OrderInfo.objects.create(order_id=order_id,
+                                             user=user,
+                                             address=address,
+                                             total_count=total_count,
+                                             total_amount=total_amount,
+                                             pay_method=pay_method,
+                                             status=status,
+                                             freight=freight)
+        except Exception as e:
+            return JsonResponse({'code': 400,
+                                 'message': '保存数据出错'})
+
+        # ④ 遍历用户要购买的商品记录，循环向 tb_order_goods 表中添加记录
+        # 从 redis 中获取用户要购买的商品信息
+        cart_helper = CartHelper(request)
+        cart_dict = cart_helper.get_redis_select_cart()
+        sku_ids = cart_dict.keys()
+
+        for sku_id in sku_ids:
+            sku = SKU.objects.get(id=sku_id)
+            count = cart_dict[sku.id]
+
+            # 判断库存是否充足
+            if count > sku.stock:
+                return JsonResponse({'code': 400,
+                                     'message': '商品库存不足'})
+
+            # 减少SKU商品库存、增加销量
+            sku.stock -= count
+            sku.sales += count
+            sku.save()
+            # 增加对应SPU商品的销量
+
+            sku.spu.sales += count
+            sku.spu.save()
+
+            # 保存订单商品信息
+            try:
+                OrderGoods.objects.create(order=order,
+                                          sku=sku,
+                                          count=count,
+                                          price=sku.price)
+            except Exception as e:
+                return JsonResponse({'code': 400,
+                                     'message': '保存数据出错'})
+
+            # 累加计算订单商品的总数量和总价格
+            total_count += total_count
+            total_amount += count * sku.price
+
+        total_amount += freight
+        order.total_count = total_count
+        order.total_amount = total_amount
+        order.save()
+
+        # ⑤ 清除用户购物车中已购买的记录
+        cart_helper.clear_redis_selected_cart()
+
+        # ⑥ 返回响应
+        return JsonResponse({'code': 0,
+                             'message': '下单成功',
+                             'order_id': order_id})
